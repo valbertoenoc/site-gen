d19a2fd7720dded320e8268cc02f5996fb84b094
valbertoenoc <valbertoenoc@gmail.com>
2026-03-20 21:02:01 -0300

    copy static/ to public/

--- src/main.py
+++ src/main.py
@@ -1,10 +1,24 @@
-from textnode import TextNode, TextType
+import os, shutil
+
+
+def rm_dir_content(path: str):
+    try:
+        print(f"Attempting directory removal: {path}")
+        shutil.rmtree(path)
+        print("Directory removed successfully")
+    except Exception as e:
+        print(f"could not remove directory: {path}", e)
+
+
+def copy_dir_content(src: str, dst: str):
+    shutil.copytree(src, dst)
 
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
-
-    print(text_node)
+    # remove public/ dir
+    rm_dir_content("public/")
+    # copy content from src to dst
+    copy_dir_content("static/", "public/")
 
 
 if __name__ == "__main__":
